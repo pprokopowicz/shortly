ae8063f1ae671dbcefeb586212fa6ebe14e88332
Piotr Prokopowicz <p.prokopowicz1@gmail.com>
2022-11-19 18:02:41 +0100

    DbConnection added

---
+++ shortly-data/src/db.rs
@@ -0,0 +1,23 @@
+use dotenv::dotenv;
+use sqlx::postgres::{PgPool, PgPoolOptions};
+use std::env;
+
+const DB_ENV_KEY: &'static str = "DATABASE_URL";
+
+pub struct DbConnection {
+    pub(super) pool: PgPool,
+}
+
+impl DbConnection {
+    pub async fn new() -> Result<DbConnection, sqlx::Error> {
+        dotenv().ok();
+
+        let db_url = env::var(DB_ENV_KEY).expect("DATABASE_URL must be set");
+        let pool = PgPoolOptions::new()
+            .max_connections(8)
+            .connect(&db_url)
+            .await?;
+
+        Ok(DbConnection { pool })
+    }
+}
